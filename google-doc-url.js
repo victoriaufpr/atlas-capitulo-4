8c0a54f67c3883c24c401100b1c820500e2c0e5e
victoriaufpr <137126119+victoriaufpr@users.noreply.github.com>
2023-06-20 02:31:46 -0300

    Create google-doc-url.js
    Repositório sheets

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kPsyg8BGkVuGzDP6CSsfEDDn9Gvy7A7jrD_f9CndyTg/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyCICGyBeEw_KWbQ8WK73DeTEB_icnK7BJ0';
